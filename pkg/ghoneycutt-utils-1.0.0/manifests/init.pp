ee67aca5693b7e1c43f06b14cc98d80dc2233c81
Garrett Honeycutt <github@garretthoneycutt.com>
2010-06-24 16:36:41 -0700

    initial publication

---
+++ pkg/ghoneycutt-utils-1.0.0/manifests/init.pp
@@ -0,0 +1,26 @@
+# Class: utils
+#
+# install useful utils on all nodes through the generic module
+# do NOT install build tools like gcc here! - those go in the buildtools module
+#
+# Sample Usage: include utils
+#
+class utils {
+
+    $utilsPackageList = [ 
+        "curl",
+        "lsof",
+        "lynx",
+        "nc",
+        "nmap",
+        "screen",
+        "strace",
+        "sysstat",
+        "tcpdump",
+        "telnet",
+        "wget"
+    ] # $utilsPackageList
+
+    package { $utilsPackageList: }
+
+} # class utils
